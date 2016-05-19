f11cdea575eb2e530d4f55de8b44064b7aff038a
edoglaz <edoglaz@yahoo.com>
2016-05-19 15:36:20 +0200

    Database toegevoegd

---
+++ app/src/main/java/com/example/edo/studiebarometer/database/Database/DatabaseHelper.java
@@ -0,0 +1,65 @@
+package com.example.edo.studiebarometer.database.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static SQLiteDatabase mSQLDB;
+    private static DatabaseHelper mInstance;			// SINGLETON TRUC
+    public static final String dbName = "cijferlijst.db";	// Naam van je DB
+    public static final int dbVersion = 1;				// Versie nr van je db.
+
+    public DatabaseHelper(Context ctx) {				// De constructor
+        super(ctx, dbName, null, dbVersion);			// super constructorgithub
+    }
+
+    public static synchronized DatabaseHelper getHelper (Context ctx){  // SYNCRONIZED TRUC
+        if (mInstance == null){
+            mInstance = new DatabaseHelper(ctx);
+            mSQLDB = mInstance.getWritableDatabase();
+        }
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + Databaseinfo.CourseTables.COURSE + " (" +
+                BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Databaseinfo.CourseColumn.NAME + " TEXT," + Databaseinfo.CourseColumn.ECTS + " TEXT," +
+                Databaseinfo.CourseColumn.PERIOD + " TEXT," + Databaseinfo.CourseColumn.GRADE + " TEXT," + Databaseinfo.CourseColumn.GEHAALD + " TEXT);"
+        );
+    }
+
+
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ Databaseinfo.CourseTables.COURSE);	 // GOOI ALLES WEG
+        onCreate(db);									 // EN CREER HET OPNIEUW
+    }
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version ){
+        super(context, name, factory, version);
+    }
+
+    public void insert(String table, String nullColumnHack, ContentValues values){
+        mSQLDB.insert(table, nullColumnHack, values);
+    }
+
+    public Cursor query(String table, String[] columns, String selection, String[] selectArgs, String groupBy, String having, String orderBy){
+        return mSQLDB.query(table, columns, selection, selectArgs, groupBy, having, orderBy);
+    }
+
+
+    public void replace(String table, String nullColumnHack, ContentValues values) {
+        mSQLDB.replace(table, nullColumnHack, values);
+    }
+}
+
+
---
+++ app/src/main/java/com/example/edo/studiebarometer/database/Database/Databaseinfo.java
@@ -0,0 +1,22 @@
+package com.example.edo.studiebarometer.database.Database;
+
+
+public class Databaseinfo {
+
+
+        public class CourseTables {
+            public static final String COURSE = "course";		// NAAM VAN JE TABEL
+        }
+
+        public class CourseColumn {
+            public static final String NAME = "name";        // Naam van gebruiker
+            public static final String ECTS = "ects";        // Aantal studiepunten
+            public static final String GRADE = "grade";           // cijfer
+            public static final String PERIOD = "period";     // periode (1/2/3/4)
+            public static final String GEHAALD = "gehaald";   //Gehaald of niet
+        }
+
+
+
+
+}
