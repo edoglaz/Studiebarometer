04370844cd39eb0aad7f21dee5ee39fbc9a20a2d
edoglaz <edoglaz@yahoo.com>
2016-05-19 16:15:21 +0200

    MainActivity en Course model alvast toegevoegd

---
+++ app/src/main/java/com/example/edo/studiebarometer/database/Databaseinfo.java
@@ -0,0 +1,23 @@
+package com.example.edo.studiebarometer.database;
+
+
+
+public class Databaseinfo {
+
+
+        public class CourseTables {
+            public static final String COURSE = "course";		// NAAM VAN JE TABEL
+        }
+
+        public class CourseColumn {
+            public static final String NAME = "name";        // naam van gebruiker
+            public static final String ECTS = "ects";        // studiepunten
+            public static final String GRADE = "grade";           // cijfer
+            public static final String PERIOD = "period";     // periodes
+            public static final String GEHAALD = "gehaald";   //Gehaald of niet
+        }
+
+
+
+
+}
